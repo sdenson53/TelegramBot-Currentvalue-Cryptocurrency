65b7963705da04d36b7ab474a7478d3b79396f79
sdenson53 <32272435+sdenson53@users.noreply.github.com>
2019-08-05 18:08:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,49 @@
 import time
 import telebot
 import requests
+from pycoingecko import CoinGeckoAPI
 
 
-TOKEN = "your telegram bot token obtained from BotFather"
+cg = CoinGeckoAPI()
+TOKEN = "648843546:AAE_-WfnWrFqjSeSiiCeMgHK1YY2AvDn07M"
 bot = telebot.TeleBot(token=TOKEN)
 
-def findat(msg):
-    # from a list of texts, it finds the one with the '@' sign
-    for i in msg:
-        if '@' in i:
-            return i
 
-@bot.message_handler(commands=['start']) # welcome message handler
+@bot.message_handler(commands=['start'])  # welcome message handler
 def send_welcome(message):
-    bot.reply_to(message, 'Hi There! \n Enter /btc to get current value'
-                          ' \n or @username to get a link to instagram account')
+    bot.reply_to(message, 'Hi There!...Enter: \n /btc : current Bitcoin value \n /eth : current Ethereum value\n'
+                          ' /ltc : current Litecoin value \n /xrp : current ripple value')
 
-@bot.message_handler(commands=['btc']) # help message handler
+
+@bot.message_handler(commands=['btc'])  # bitcoin
 def send_welcome(message):
     api = requests.get(url='https://blockchain.info/tobtc?currency=USD&value=1')
     dd = api.json()
-    xx = 1/dd
-    bot.reply_to(message, xx)
+    xx = 1 / dd
+    b = '$' + str(xx)
+    bot.reply_to(message, b)
 
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
-# lambda function finds messages with the '@' sign in them
-# in case msg.text doesn't exist, the handler doesn't process it
-def at_converter(message):
-    texts = message.text.split()
-    at_text = findat(texts)
-    if at_text == '@':  # in case it's just the '@', skip
-        pass
-    else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
-        bot.reply_to(message, insta_link)
+
+@bot.message_handler(commands=['eth'])  # ethereum
+def send_welcome(message):
+    a = str(cg.get_price(ids='ethereum', vs_currencies='usd'))
+    b = '$' + a[21:27]
+    bot.reply_to(message, b)
+
+
+@bot.message_handler(commands=['ltc'])  # litecoin
+def send_welcome(message):
+    a = str(cg.get_price(ids='litecoin', vs_currencies='usd'))
+    b = '$' + a[21:27]
+    bot.reply_to(message, b)
+
+
+@bot.message_handler(commands=['xrp'])  # ripple
+def send_welcome(message):
+    a = str(cg.get_price(ids='ripple', vs_currencies='usd'))
+    b = '$' + a[19:26]
+    bot.reply_to(message, b)
+
 
 while True:
     try:
